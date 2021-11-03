ccd03fa0bf61f2a3569eca0bded056fc01891458
karanikathungu-crypto <kathungudev@gmail.com>
2021-11-03 20:27:50 +0300

    Add the business methods to retrieve details for hospital to display

--- app/src/main/java/com/moringaschool/medi_finder/ui/HospitalDetailsFragment.java
+++ app/src/main/java/com/moringaschool/medi_finder/ui/HospitalDetailsFragment.java
@@ -7,60 +7,87 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moringaschool.medi_finder.R;
+import com.moringaschool.medi_finder.models.Business;
+import com.moringaschool.medi_finder.models.Category;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HospitalDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HospitalDetailsFragment extends Fragment {
+import org.parceler.Parcels;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class HospitalDetailsFragment extends Fragment implements View.OnClickListener{
+
+    @BindView(R.id.hospitalImageView) ImageView mImageLabel;
+    @BindView(R.id.hospitalNameTextView) TextView mNameLabel;
+    @BindView(R.id.categoryTextView) TextView mCategoriesLabel;
+    @BindView(R.id.workingHoursTextView) TextView mWorkingHoursLabel;
+    @BindView(R.id.reviewCountTextView) TextView mReviewCountLabel;
+    @BindView(R.id.ratingTextView) TextView mRatingLabel;
+    @BindView(R.id.distanceTextView) TextView mDistanceLabel;
+    @BindView(R.id.websiteTextView) TextView mWebsiteLabel;
+    @BindView(R.id.phoneTextView) TextView mPhoneLabel;
+    @BindView(R.id.addressTextView) TextView mAddressLabel;
+    @BindView(R.id.saveHospitalButton) TextView mSaveRestaurantButton;
+
+    private Business mHospital;
 
     public HospitalDetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HospitalDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HospitalDetailsFragment newInstance(String param1, String param2) {
-        HospitalDetailsFragment fragment = new HospitalDetailsFragment();
+    public static HospitalDetailsFragment newInstance(Business hospital) {
+        HospitalDetailsFragment hospitalDetailsFragment = new HospitalDetailsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("hospital", Parcels.wrap(hospital));
+        hospitalDetailsFragment.setArguments(args);
+        return hospitalDetailsFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        assert getArguments()!= null;
+        mHospital = Parcels.unwrap(getArguments().getParcelable("hospital"));
+
+        mSaveRestaurantButton.setOnClickListener(this);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_hospital_details, container, false);
+        ButterKnife.bind(this, view);
+        Picasso.get().load(mHospital.getImageUrl()).into(mImageLabel);
+
+        List<String> categories = new ArrayList<>();
+
+        for(Category category: mHospital.getCategories()){
+            categories.add(category.getTitle());
+        }
+
+        mNameLabel.setText(mHospital.getName());
+        mCategoriesLabel.setText(android.text.TextUtils.join(",", categories));
+        mWorkingHoursLabel.setText(Boolean.toString(mHospital.getIsClosed()));
+        mReviewCountLabel.setText(Integer.toString(mHospital.getReviewCount()));
+        mRatingLabel.setText(Double.toString(mHospital.getRating()));
+        mWebsiteLabel.setText(mHospital.getUrl());
+        mPhoneLabel.setText(mHospital.getPhone());
+        mDistanceLabel.setText(Double.toString(mHospital.getDistance()));
+        mAddressLabel.setText(mHospital.getLocation().toString());
+
         return inflater.inflate(R.layout.fragment_hospital_details, container, false);
     }
 }

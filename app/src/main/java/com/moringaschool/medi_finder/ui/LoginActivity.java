41f7e189303646b03d33c21d6af02be3c3906de6
KyleTrippK <85233565+KyleTrippK@users.noreply.github.com>
2021-11-04 10:33:10 +0300

    Update LoginActivity.java
    merge kyle with development

--- app/src/main/java/com/moringaschool/medi_finder/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/medi_finder/ui/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.moringaschool.medi_finder.ui;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.moringaschool.medi_finder.MainActivity;
 import com.moringaschool.medi_finder.R;
 
 import butterknife.BindView;
@@ -22,6 +33,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @BindView(R.id.createAccountTextView) TextView mCreateAccountTextView;
     @BindView(R.id.firebaseProgressBar) ProgressBar mSignInProgressBar;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +45,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mLogInButton.setOnClickListener(this);
         mCreateAccountTextView.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
     }
 
     @Override
@@ -59,6 +88,31 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if(password.equals("")){
             mPasswordEditText.setError("Please Fill your Password");
             return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgressbar();
+                        Log.d(TAG, "SignInWithEmailAndPassWord:onComplete" + task.isSuccessful());
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 

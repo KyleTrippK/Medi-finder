9435084b42635a2618d190295b18a07440c1782d
lanarokip <lankipkoecharo@gmail.com>
2021-11-04 17:43:57 +0300

    initial commit

--- app/src/main/java/com/moringaschool/medi_finder/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/medi_finder/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.moringaschool.medi_finder;
+package com.moringaschool.medi_finder.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.moringaschool.medi_finder.R;
 
 public class MainActivity extends AppCompatActivity {
 
